4c5c44a8db6d9c4f449253baae38c830d4c5dccf
Roger Woodroofe <rogyw@yahoo.co.nz>
2016-05-23 14:15:10 +1200

    feat: add initial event data

---
+++ js/eventdata.js
@@ -0,0 +1,231 @@
+eventType = [{
+	"name": "SummerNav Evening",
+	"start-first": "17:30",
+	"start-last": "18:45",
+	"course-close": "19:30",
+	"notes": "Compass not usually required."
+}, {
+	"name": "SummerNav Night",
+	"start-first": "20:30",
+	"start-last": "20:30",
+	"course-close": "21:15",
+	"notes": "Bring a good torch or headlamp; Kids can start at dusk; Compass recommended."
+}, {
+	"name": "AOA Forest",
+	"start-first": "10:00",
+	"start-last": "12:30",
+	"course-close": "14:30",
+	"notes": "Recommend bringing whistle and compass. Bring a good torch or headlamp; Kids can start at dusk; "
+},{
+	"name": "Winter Sprint Afternoon",
+	"start-first": "16:00",
+	"start-last": "16:30",
+	"course-close": "17:00",
+	"notes": "Sprint, Jog, or Walk. Expected fastest time of 12 minutes for each course."
+},{
+	"name": "WinterNav Afternoon",
+	"start-first": "13:00",
+	"start-last": "14:15",
+	"course-close": "16:00",
+	"notes": "Compass not usually required."
+}];
+
+events = [{
+	"date": "2016-10-27",
+	"venue": "Cornwall Park",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2016-11-02 Nov",
+	"venue": "Hamlins Hill",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2016-11-10 Nov",
+	"venue": "Auckland Domain",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2016-11-16 Nov",
+	"venue": "Highbrook",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2016-11-22 Nov",
+	"venue": "Western Springs",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2016-11-30 Nov",
+	"venue": "Craigavon",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2016-12-08 Dec",
+	"venue": "Ambury Park",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": "No Dogs Permitted."
+}, {
+	"date": "2016-12-14 Dec",
+	"venue": "Epsom Campus",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-01-25 Jan",
+	"venue": "Pt England",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-01-31 Jan",
+	"venue": "Alexandra Park & Schools",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-02-08 Feb",
+	"venue": "Lloyd Elsmore",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-02-14 Feb",
+	"venue": "Cornwall Park",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-02-18 Feb",
+	"venue": "(Night Event) Ambury Park",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": "No Dogs Permitted."
+}, {
+	"date": "2017-02-23 Feb",
+	"venue": "Panmure Basin",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-03-01 Mar",
+	"venue": "Selfs Farm",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-03-07 Mar",
+	"venue": "Auckland Domain",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-03-16 Mar",
+	"venue": "Ellerslie Racecourse",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-03-22 Mar",
+	"venue": "Western Springs",
+	"event-type": "SummerNav Evening",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-05-20 May",
+	"venue": "Cornwall Park",
+	"event-type": "WinterNav Afternoon",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-06-17 Jun",
+	"venue": "Craigavon",
+	"event-type": "WinterNav Afternoon",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-08-19 Aug",
+	"venue": "Auckland Domain",
+	"event-type": "Winter Sprint Afternoon",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}, {
+	"date": "2017-09-16 Sep",
+	"venue": "Western Springs",
+	"event-type": "Winter Sprint Afternoon",
+	"registration-location": {
+				lat: ,
+				lng:
+			},
+	"notes": ""
+}];
