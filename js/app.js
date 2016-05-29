0b906b29ac500c46746bb08157cd7710f6e6cf10
Roger Woodroofe <rogyw@yahoo.co.nz>
2016-05-29 21:23:10 +1200

    feat: change map marker display based on filter search

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // set jshint to ignore console, alert, etc
 /* jshint devel: true */
 // set jshint to ignore external globals
-/* global ko, eventsJSON: false */
+/* global ko, google, eventsJSON: false */
 
 /* ======================================================= */
 /* List */
@@ -25,8 +25,20 @@ var oEvent = function(data) {
 		self.courseClose = ko.observable(data.courseClose);
 		self.registrationCoord = ko.observable(data.registrationCoord);
 		self.notes = ko.observable(data.notes);
+
+		self.data = data; //store imported raw data in oEvent for reference
+		self.mapMarker = createEventMarker(data);
 	}
 };
+
+
+function enableMapMarker(element) {
+	element.mapMarker.setMap(map);
+}
+
+function disableMapMarker(element) {
+	element.mapMarker.setMap(null);
+}
 
 
 /**
@@ -37,11 +49,11 @@ var viewModel = function() {
 	//self represents the ViewModel this
 	var self = this;
 
-	self.eventList = ko.observableArray([]);
+	self.eventsList = ko.observableArray();
 	self.filter = ko.observable("");
 
 	eventsJSON.forEach(function(eventItem) {
-		self.eventList.push(new oEvent(eventItem));
+		self.eventsList.push(new oEvent(eventItem));
 	});
 
 	//filter the items using the filter text
@@ -49,14 +61,133 @@ var viewModel = function() {
 	self.filteredEvents = ko.dependentObservable(function() {
 		var filter = this.filter().toLowerCase();
 		if (!filter) {
-			//return all the items
-			return this.eventList();
+			//enable and return all the items
+			this.eventsList().forEach(enableMapMarker);
+			return this.eventsList();
 		} else {
-			return ko.utils.arrayFilter(this.eventList(), function(event) {
-				return (event.name().toLowerCase().search(filter) !== -1);
+			return ko.utils.arrayFilter(this.eventsList(), function(event) {
+				if (event.name().toLowerCase().search(filter) !== -1) {
+					enableMapMarker(event);
+					return true;
+				} else {
+					disableMapMarker(event);
+					return false;
+				}
 			});
 		}
 	}, self);
 };
 
-ko.applyBindings(new viewModel());
+
+/* ======================================================= */
+/* Map
+/* ======================================================= */
+
+var map;
+
+/**
+ * creates an object containing Title and Content for a marker based on provided data object
+ * @param  {object} data JSON data object containing event information
+ * @return {object} Object containing Title and HTML for Marker Content
+ */
+function getEventMarkerContent(data) {
+
+	var markerContent = {};
+	var infoContent;
+
+	// Build the Event Title
+	markerContent.title = data.series + " - " + data.name;
+
+	//Build the marker pop up window content
+	infoContent = '<div class="map-info">';
+	infoContent += "<h3>" + data.name + "</h3>";
+	infoContent += "<h4>" + data.series + "</h4>";
+	infoContent += "<ul>";
+	infoContent += "<li><strong>Date: " + data.date + "</strong></li>";
+	infoContent += "<li>Start between: " + data.startFirst + " - " + data.startLast + "</li>";
+	infoContent += "<li>Course closure Time (Check notice board): " + data.courseClose + "</li>";
+	if (data.notes !== "") {
+		infoContent += "<li>Note: " + data.notes + "</li>";
+	}
+	infoContent += "</ul></div>";
+	markerContent.infoContent = infoContent;
+
+	return markerContent;
+}
+
+
+/**
+ * Function to run within a ForEach on Event Marker List. Create an event marker based on each element within the array.
+ * @param  {object} element JSON object containing event data
+ * @param  {integer} index   current index of array (optional)
+ * @param  {[type]} array   event Array (optional)
+ * @return {boolean}  Returns false on success, true if failure
+ */
+function createEventMarker(element) {
+
+	var markerContent = getEventMarkerContent(element);
+	var newMarker = new google.maps.Marker({
+		position: element.registrationCoord,
+		map: map,
+		title: markerContent.title,
+	});
+
+	newMarker.setMap(map);
+
+	// infoWindows are the little helper windows that open when you click
+	// or hover over a pin on a map. They usually contain more information
+	// about a location.
+	var infoWindow = new google.maps.InfoWindow({
+		content: markerContent.infoContent
+	});
+
+	// open Information Window when Marker clicked
+	google.maps.event.addListener(newMarker, 'click', function() {
+		infoWindow.open(map, newMarker);
+	});
+
+	return newMarker;
+}
+
+
+/**
+ * Repositions and resizes map view based on markers.
+ */
+function resizeMap() {
+
+	//Start off with null LatLngBounds (i.e. reset)
+	var bounds = new google.maps.LatLngBounds();
+
+	//Add each of the currently visible mapMarkers to the bounds
+	for (var i = 0, l = viewModel.filteredEvents().length; i < l; i++) {
+		var position = viewModel.filteredEvents()[i].mapMarker.position;
+		bounds.extend(position);
+	}
+
+	// center the map on the center of bounds
+	map.setCenter(bounds.getCenter());
+
+	// Zoom map to fit the new bounds
+	map.fitBounds(bounds);
+}
+
+
+/**
+ * Initialises the Map and places all Event Markers on Map
+ * @return {boolean} Returns false on success, true if failure
+ */
+function initMap() {
+
+	var mapOptions = {
+		center: { lat: -36.9001229, lng: 174.7826388 },
+		zoom: 12
+	};
+
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	// Vanilla JS way to listen for resizing of the window
+	// and adjust map bounds
+	window.addEventListener('resize', resizeMap);
+
+	ko.applyBindings(new viewModel());
+}
