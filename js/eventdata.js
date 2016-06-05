b9be89057a7bad303d4f21a4fb264f9ff21ea05d
Roger Woodroofe <rogyw@yahoo.co.nz>
2016-06-05 22:12:10 +1200

    fix: add updated event data structure

--- js/eventdata.js
+++ js/eventdata.js
@@ -1,156 +1,156 @@
 "use strict";
 
 var eventsJSON = [{
-	"dateUTC": "2016-10-27 05:30",
+	"dateUTC": "2016-10-27 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Cornwall Park - Archery Carpark",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-10-27 04:30",
+	"startLastUTC": "2016-10-27 05:45",
+	"courseCloseUTC": "2016-10-27 06:30",
 	"registrationCoord": {
 		lat: -36.903482,
 		lng: 174.784422
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2016-11-02 05:30",
+	"dateUTC": "2016-11-02 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Hamlins Hill - Carpark",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-11-02 04:30",
+	"startLastUTC": "2016-11-02 05:45",
+	"courseCloseUTC": "2016-11-02 06:30",
 	"registrationCoord": {
 		lat: -36.923982,
 		lng: 174.831291
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2016-11-10 05:30",
+	"dateUTC": "2016-11-10 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Auckland Domain - Cricket Grandstand",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-11-10 04:30",
+	"startLastUTC": "2016-11-10 05:45",
+	"courseCloseUTC": "2016-11-10 06:30",
 	"registrationCoord": {
 		lat: -36.863269,
 		lng: 174.771962
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2016-11-16 05:30",
+	"dateUTC": "2016-11-16 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Highbrook - Pukekiwiriki Entrance",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-11-16 04:30",
+	"startLastUTC": "2016-11-16 05:45",
+	"courseCloseUTC": "2016-11-16 06:30",
 	"registrationCoord": {
 		lat: -36.944184,
 		lng: 174.871469
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2016-11-22 05:30",
+	"dateUTC": "2016-11-22 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Western Springs - Zoo Carpark",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-11-22 04:30",
+	"startLastUTC": "2016-11-22 05:45",
+	"courseCloseUTC": "2016-11-22 06:30",
 	"registrationCoord": {
 		lat: -36.865097,
 		lng: 174.720507
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2016-11-30 05:30",
+	"dateUTC": "2016-11-30 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Craigavon",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-11-30 04:30",
+	"startLastUTC": "2016-11-30 05:45",
+	"courseCloseUTC": "2016-11-30 06:30",
 	"registrationCoord": {
 		lat: -36.928414,
 		lng: 174.690347
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2016-12-08 05:30",
+	"dateUTC": "2016-12-08 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Ambury Park",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-12-08 04:30",
+	"startLastUTC": "2016-12-08 05:45",
+	"courseCloseUTC": "2016-12-08 06:30",
 	"registrationCoord": {
 		lat: -36.945714,
 		lng: 174.761737
 	},
 	"notes": "No Dogs Permitted. Compass not usually required."
 }, {
-	"dateUTC": "2016-12-14 05:30",
+	"dateUTC": "2016-12-14 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Epsom Campus - St Andrews Rd Entrance",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2016-12-14 04:30",
+	"startLastUTC": "2016-12-14 05:45",
+	"courseCloseUTC": "2016-12-14 06:30",
 	"registrationCoord": {
 		lat: -36.885772,
 		lng: 174.767751
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-01-25 05:30",
+	"dateUTC": "2017-01-25 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Pt England",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-01-25 04:30",
+	"startLastUTC": "2017-01-25 05:45",
+	"courseCloseUTC": "2017-01-25 06:30",
 	"registrationCoord": {
 		lat: -36.883369,
 		lng: 174.871948
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-01-31 05:30",
+	"dateUTC": "2017-01-31 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Alexandra Park Raceway - Campbell Crescent Entrance",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-01-31 04:30",
+	"startLastUTC": "2017-01-31 05:45",
+	"courseCloseUTC": "2017-01-31 06:30",
 	"registrationCoord": {
 		lat: -36.889485,
 		lng: 174.776607
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-02-08 05:30",
+	"dateUTC": "2017-02-08 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Lloyd Elsmore - Rugby Carpark",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-02-08 04:30",
+	"startLastUTC": "2017-02-08 05:45",
+	"courseCloseUTC": "2017-02-08 06:30",
 	"registrationCoord": {
 		lat: -36.907876,
 		lng: 174.901683
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-02-14 05:30",
+	"dateUTC": "2017-02-14 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Cornwall Park",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-02-14 04:30",
+	"startLastUTC": "2017-02-14 05:45",
+	"courseCloseUTC": "2017-02-14 06:30",
 	"registrationCoord": {
 		lat: -36.898066,
 		lng: 174.788799
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-02-18 05:30",
+	"dateUTC": "2017-02-18 04:30",
 	"series": "Auckland SummerNav",
 	"name": "(Night Event) Ambury Park",
-	"startFirst": "20:30",
-	"startLast": "20:30",
-	"courseClose": "21:15",
+	"startFirstUTC": "2017-02-18 07:30",
+	"startLastUTC": "2017-02-18 08:15",
+	"courseCloseUTC": "2017-02-18 9:30",
 	"registrationCoord": {
 		lat: -36.945714,
 		lng: 174.761737
@@ -158,60 +158,60 @@ var eventsJSON = [{
 	"notes": "Bring a good torch or headlamp; Kids can start at dusk; Compass recommended. No Dogs Permitted."
 
 }, {
-	"dateUTC": "2017-02-23 05:30",
+	"dateUTC": "2017-02-23 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Panmure Basin",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-02-23 04:30",
+	"startLastUTC": "2017-02-23 05:45",
+	"courseCloseUTC": "2017-02-23 06:30",
 	"registrationCoord": {
 		lat: -36.908449,
 		lng: 174.848238
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-03-01 05:30",
+	"dateUTC": "2017-03-01 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Selfs Farm",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-03-01 04:30",
+	"startLastUTC": "2017-03-01 05:45",
+	"courseCloseUTC": "2017-03-01 06:30",
 	"registrationCoord": {
 		lat: -36.982238,
 		lng: 174.826492
 	},
 	"notes": "Working Private Farm. Compass not usually required."
 }, {
-	"dateUTC": "2017-03-07 05:30",
+	"dateUTC": "2017-03-07 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Auckland Domain - Maunsell Rd Carpark",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-03-07 04:30",
+	"startLastUTC": "2017-03-07 05:45",
+	"courseCloseUTC": "2017-03-07 06:30",
 	"registrationCoord": {
 		lat: -36.862014,
 		lng: 174.778579
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-03-16 05:30",
+	"dateUTC": "2017-03-16 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Ellerslie Racecourse",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-03-16 04:30",
+	"startLastUTC": "2017-03-16 05:45",
+	"courseCloseUTC": "2017-03-16 06:30",
 	"registrationCoord": {
 		lat: -36.888302,
 		lng: 174.803875
 	},
 	"notes": "Compass not usually required."
 }, {
-	"dateUTC": "2017-03-22 05:30",
+	"dateUTC": "2017-03-22 04:30",
 	"series": "Auckland SummerNav",
 	"name": "Western Springs Stadium",
-	"startFirst": "17:30",
-	"startLast": "18:45",
-	"courseClose": "19:30",
+	"startFirstUTC": "2017-03-22 04:30",
+	"startLastUTC": "2017-03-22 05:45",
+	"courseCloseUTC": "2017-03-22 06:30",
 	"registrationCoord": {
 		lat: -36.865933,
 		lng: 174.727192
@@ -221,9 +221,9 @@ var eventsJSON = [{
 	"dateUTC": "2017-05-20 01:00",
 	"series": "Auckland WinterNav",
 	"name": "Cornwall Park",
-	"startFirst": "13:00",
-	"startLast": "14:15",
-	"courseClose": "16:00",
+	"startFirstUTC": "2017-05-20 01:00",
+	"startLastUTC": "2017-05-20 02:15",
+	"courseCloseUTC": "2017-05-20 04:00",
 	"registrationCoord": {
 		lat: -36.898066,
 		lng: 174.788799
@@ -233,9 +233,9 @@ var eventsJSON = [{
 	"dateUTC": "2017-06-17 01:00",
 	"series": "Auckland WinterNav",
 	"name": "Craigavon",
-	"startFirst": "13:00",
-	"startLast": "14:15",
-	"courseClose": "16:00",
+	"startFirstUTC": "2017-06-17 01:00",
+	"startLastUTC": "2017-06-17 02:15",
+	"courseCloseUTC": "2017-06-17 04:00",
 	"registrationCoord": {
 		lat: -36.928414,
 		lng: 174.690347
@@ -245,9 +245,9 @@ var eventsJSON = [{
 	"dateUTC": "2017-08-19 01:00",
 	"series": "Auckland WinterNav",
 	"name": "Auckland Domain - Cricket Grandstand",
-	"startFirst": "13:00",
-	"startLast": "14:15",
-	"courseClose": "16:00",
+	"startFirstUTC": "2017-08-19 01:00",
+	"startLastUTC": "2017-08-19 02:15",
+	"courseCloseUTC": "2017-08-19 04:00",
 	"registrationCoord": {
 		lat: -36.863269,
 		lng: 174.771962
@@ -257,9 +257,9 @@ var eventsJSON = [{
 	"dateUTC": "2017-09-16 01:00",
 	"series": "Auckland WinterNav",
 	"name": "Western Springs - Zoo Carpark",
-	"startFirst": "13:00",
-	"startLast": "14:15",
-	"courseClose": "16:00",
+	"startFirstUTC": "2017-09-16 01:00",
+	"startLastUTC": "2017-09-16 02:15",
+	"courseCloseUTC": "2017-09-16 04:00",
 	"registrationCoord": {
 		lat: -36.865097,
 		lng: 174.720507
