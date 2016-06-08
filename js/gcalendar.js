e7824bf39b52d9fdc8afd73ce1515648b4230581
Roger Woodroofe <rogyw@yahoo.co.nz>
2016-06-08 17:00:58 +1200

    feat: change Google Calendar API call error to display in alert window.

--- js/gcalendar.js
+++ js/gcalendar.js
@@ -26,7 +26,9 @@ var DEBUG_GCALENDAR = true;
  * Check if current user has authorized this application.
  */
 function gCalendarCheckAuth() {
-	if (DEBUG_GCALENDAR) { console.log("gCalendarCheckAuth: Start - Check if current user has authorized this application."); }
+	if (DEBUG_GCALENDAR) {
+		console.log("gCalendarCheckAuth: Start - Check if current user has authorized this application.");
+	}
 	gapi.auth.authorize({
 		'client_id': GCALENDAR_CLIENT_ID,
 		'scope': GCALENDAR_SCOPES.join(' '),
@@ -41,16 +43,22 @@ function gCalendarCheckAuth() {
  * @param {Object} authResult Authorization result.
  */
 function gCalendarHandleAuthResult(authResult) {
-	if (DEBUG_GCALENDAR) { console.log("gCalendarHandleAuthResult: Start - Handle response from authorization server."); }
+	if (DEBUG_GCALENDAR) {
+		console.log("gCalendarHandleAuthResult: Start - Handle response from authorization server.");
+	}
 	var authorizeElement = document.getElementsByClassName('g-calendar-button');
 	console.log(authorizeElement[0]);
 	if (authResult && !authResult.error) {
 		// Hide auth UI, then load client library.
-		if (DEBUG_GCALENDAR) { console.log("gCalendarHandleAuthResult: (authResult && !authResult.error) Hide auth UI, then load client library."); }
+		if (DEBUG_GCALENDAR) {
+			console.log("gCalendarHandleAuthResult: (authResult && !authResult.error) Hide auth UI, then load client library.");
+		}
 		authorizeElement[0].style.display = 'none';
 		gCalendarLoadCalendarApi();
 	} else {
-		if (DEBUG_GCALENDAR) { console.log("gCalendarHandleAuthResult: Show auth UI, allowing the user to initiate authorization by clicking authorize button."); }
+		if (DEBUG_GCALENDAR) {
+			console.log("gCalendarHandleAuthResult: Show auth UI, allowing the user to initiate authorization by clicking authorize button.");
+		}
 		// Show auth UI, allowing the user to initiate authorization by
 		// clicking authorize button.
 		authorizeElement[0].style.display = 'inline';
@@ -64,7 +72,9 @@ function gCalendarHandleAuthResult(authResult) {
  * @param {Event} event Button click event.
  */
 function gCalendarHandleAuthClick(event) {
-	if (DEBUG_GCALENDAR) { console.log("gCalendarHandleAuthClick: Start - Initiate auth flow in response to user clicking authorize button."); }
+	if (DEBUG_GCALENDAR) {
+		console.log("gCalendarHandleAuthClick: Start - Initiate auth flow in response to user clicking authorize button.");
+	}
 	if (typeof gapi === 'undefined') {
 		alert("Sorry, Google Calendar has failed to load and can't be accessed. Try reloading this page.");
 	} else {
@@ -83,11 +93,17 @@ function gCalendarHandleAuthClick(event) {
  * Load Google Calendar client library.
  */
 function gCalendarLoadCalendarApi() {
-	if (DEBUG_GCALENDAR) { console.log("gCalendarLoadCalendarApi: Start - Load Google Calendar client library"); }
+	if (DEBUG_GCALENDAR) {
+		console.log("gCalendarLoadCalendarApi: Start - Load Google Calendar client library");
+	}
 	gapi.client.load('calendar', 'v3', gCalendarInsertEvent);
 }
 
 
+/**
+ * makes a request to Google Calendar to insert an event into Primary Calendar of current user
+ * requires global gCalendarEvent object
+ */
 function gCalendarInsertEvent() {
 	// Refer to the JavaScript quickstart on how to setup the environment:
 	// https://developers.google.com/google-apps/calendar/quickstart/js
@@ -104,22 +120,26 @@ function gCalendarInsertEvent() {
 		'resource': gCalendarEvent
 	});
 
-	if (DEBUG_GCALENDAR) { console.log("gCalendarInsertEvent: request execute"); }
+	if (DEBUG_GCALENDAR) {
+		console.log("gCalendarInsertEvent: request execute");
+	}
 
 	request.execute(function(value) {
 		if (DEBUG_GCALENDAR) {
 			console.log("gCalendarInsertEvent: value=");
 			console.log(value);
 		}
-
+		var resultText;
 		if (value.error !== undefined) {
-			var errorMessage = "Google Calendar API Request Error: " + value.error.code + " - " + value.error.message;
-			console.log("errorMessage");
-			replaceAuthorizeElement(errorMessage);
+			resultText = "Sorry, there was an error trying to add the event into Google Calendar. Google Calendar API Error: " + value.error.code + " - " + value.error.message;
+			alert(resultText);
+			console.log(resultText);
 		} else {
-			var result = 'Event added to your calendar: <a href="' + value.htmlLink + '" target="_blank">View</a>';
-			replaceAuthorizeElement(result);
-			if (DEBUG_GCALENDAR) { console.log(result); }
+			resultText = 'Success! Event has been added to your calendar: <a href="' + value.htmlLink + '" target="_blank">View</a>';
+			replaceAuthorizeElement(resultText);
+			if (DEBUG_GCALENDAR) {
+				console.log(resultText);
+			}
 		}
 	});
 
